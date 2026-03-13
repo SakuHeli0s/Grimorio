b4f202515a75cf5ed89e5af7307221e0e1d12fee
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-13 14:53:41 +0100

    Codigo formulario empezado

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 //CARGAR LOS ID DEL HTML
 
 const inputForm = document.getElementById('input-form')
+const formContainer = document.getElementById('form-container')
 const itemType = document.getElementById('item-type')
 const textDescription = document.getElementById('text-description')
+const grimoireList = document.getElementById('grimoire-list')
 const btnAdd = document.getElementById('btn-add')
 
+//CODIGO FORMULARIO
+
+function añadirObjeto (evento){
+    evento.preventDefault()
+
+    const nombreObjeto = inputForm.value
+    const tipoObjeto = itemType.value
+    const descripcionObjeto = textDescription.value
+
+}
+
+formContainer.addEventListener('submit', añadirObjeto)
