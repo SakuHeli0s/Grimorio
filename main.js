9dec55b3890d16ae99976824aa2fbbd2ea94c12f
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-13 14:16:31 +0100

    Comienzo del js

---
+++ main.js
@@ -0,0 +1,7 @@
+//CARGAR LOS ID DEL HTML
+
+const inputForm = document.getElementById('input-form')
+const itemType = document.getElementById('item-type')
+const textDescription = document.getElementById('text-description')
+const btnAdd = document.getElementById('btn-add')
+
